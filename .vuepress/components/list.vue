d83c394153dc616aa54a35669216620987d1888a
张先荣 <zxr2016@mail.ustc.edu.cn>
2021-01-21 19:26:58 +0800

    提交

---
+++ .vuepress/components/list.vue
@@ -0,0 +1,293 @@
+<template>
+    <div class="list-page">
+        <section class="list-center">
+            <h2>简介</h2>
+            <p>数据集是推动自然语言处理技术进步的基石。目前的许多技术研发仅关注模型在单一数据集上的效果，然而自然语言处理技术在大规模产业化的应用中，面临着多领域、多场景等诸多挑战。因此，我们亟需更加全面的数据集合以应对这些挑战。千言项目针对每个自然语言处理问题，均收集和整理多个开源数据集，进行统一的处理并提供统一的测评方式。千言项目期望从准确性、泛化性和鲁棒性等多角度对模型效果进行综合评价。目前，千言项目已经针对7个任务，汇集了来自11所高校和企业的22个开源数据集。未来，希望有更多的数据集作者能够参与共建千言项目，共同推动中文信息处理技术的进步，建设世界范围的中文信息处理影响力。</p>
+            <div class="list-box">
+                <a class="list-box-item" v-for="(item, index) in boxData" :href="item.link" target="_blank">
+                    <div>
+                        <img :src="item.img">
+                    </div>
+                    <div class="item-box-item-content">
+                        <p>{{item.title}}</p>
+                        <p>{{item.desc}}</p>
+                    </div>
+                </a>
+            </div>
+        </section>
+        <section class="list-center">
+            <h3>数据集作者（以下作者按照拼音排序）</h3>
+            <table width="100%" class="list-table">
+                <tr v-for="(row, rowIndex) in tableData">
+                    <td v-for="(column, columnIndex) in row" width="33.33333%">
+                        <div>
+                            <span>{{column.name}}</span>
+                            <span>{{column.unit}}</span>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <ul class="list-display">
+                <li v-for="(val, valIndex) in list">
+                    <span>{{val.name}}</span>
+                    <span>{{val.unit}}</span>
+                </li>
+            </ul>
+        </section>
+        <section class="join-section">
+            <section class="list-center">
+                <h2>加入我们</h2>
+                <p>我们希望有更多的数据集作者能够参与共建千言项目，共同推动中文信息处理技术的进步，建设世界范围的中文信息处理影响力。未来3年，我们希望面向20个任务，收集100个中文自然语言处理数据集。</p>
+                <h3>希望您能够与我们联系：</h3>
+                <div class="rule">
+                    <span class="rule-item" v-for="(item, index) in ruleData">{{item}}</span>
+                </div>
+            </section>
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            boxData: [
+                {
+                    title: '情感分析',
+                    desc: '针对句子级情感分类、评价对象级情感分类和观点抽取三个子任务，收集和整理了ChnSentiCorp、NLPCC14-SC、SemEval16-ABSA、COTE四个数据集。',
+                    img: require('../assets/sentiment-analysis.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/50/?isFromLuge=1'
+                },
+                {
+                    title: '阅读理解',
+                    desc: '针对抽取式阅读理解和观点式阅读理解两个子任务，收集和整理了DuReader_robust、DuReader_yesno两个数据集。',
+                    img: require('../assets/mrc.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/49/?isFromLuge=1'
+                },
+                {
+                    title: '开放域对话',
+                    desc: '针对多种场景的对话任务：包括网络日常对话，情感对话，知识对话和有目标的开放域对话等任务，收集和整理了来自工业界和学术界的七个对话数据集。',
+                    img: require('../assets/conversation.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/48/?isFromLuge=1'
+                },
+                {
+                    title: '文本相似度',
+                    desc: '针对多个领域数据和模型鲁棒性的问题，收集和整理了LCQMC, BQ Corpus 和PAWS-X（中文）三个数据集。',
+                    img: require('../assets/text-match.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/45/?isFromLuge=1'
+                },
+                {
+                    title: '语义解析',
+                    desc: '针对不同的数据库形式和SQL查询语句的复杂度，收集和整理了Spider、Text-to-SQL和DuSQL三个数据集。',
+                    img: require('../assets/semantic-parsing.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/47/?isFromLuge=1'
+                },
+                {
+                    title: '机器同传',
+                    desc: '机器同传结合了机器翻译、语音识别和语音合成等多种技术，整理了BSTC数据集，包含针对中英、英西两个方向共四项任务。',
+                    img: require('../assets/translate.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/44/?isFromLuge=1'
+                },
+                {
+                    title: '信息抽取',
+                    desc: '针对关系抽取和事件抽取两个子任务，收集和整理了DuIE和DuEE两个数据集。',
+                    img: require('../assets/information-extraction.png'),
+                    link: 'https://aistudio.baidu.com/aistudio/competition/detail/46/?isFromLuge=1'
+                }
+            ],
+            tableData: [
+                [{name: '陈清财等', unit: '（哈尔滨工业大学(深圳)）'}, {name: '黄民烈等', unit: '（清华大学）'}, {name: '刘云峰等', unit: '（追一科技）'}],
+                [{name: '柳厅文等', unit: '（中科院信工所）'}, {name: '史树明等', unit: '（腾讯）'}, {name: '尚利锋等', unit: '（华为）'}],
+                [{name: '谭松波等', unit: '（联想）'}, {name: '吴华等', unit: '（百度）'}, {name: '周明等', unit: '（微软）'}],
+                [{name: '赵妍妍等', unit: '（哈尔滨工业大学）'}, {name: '张岳等', unit: '（西湖大学）'}, {name: '', unit: ''}]
+            ],
+            list: [
+                {name: '陈清财等', unit: '(哈尔滨工业大学(深圳))'}, {name: '黄民烈等', unit: '(清华大学)'}, {name: '刘云峰等', unit: '(追一科技)'},
+                {name: '柳厅文等', unit: '(中科院信工所)'}, {name: '史树明等', unit: '(腾讯)'}, {name: '尚利锋等', unit: '(华为)'},
+                {name: '谭松波等', unit: '(联想)'}, {name: '吴华等', unit: '(百度)'}, {name: '周明等', unit: '(微软)'},
+                {name: '赵妍妍等', unit: '(哈尔滨工业大学)'}, {name: '张岳等', unit: '(西湖大学)'}
+            ],
+            ruleData: [
+                '将您的数据集加入已有任务',
+                '增加新的任务并加入您的数据集',
+                '欢迎您提出更多的宝贵意见'
+            ]
+        };
+    }
+};
+</script>
+<style lang="stylus" scoped>
+.list-page
+    width 100%
+    padding 10px 40px 0 40px
+    box-sizing border-box
+.list-center
+    max-width 1000px
+    width 100%
+    margin auto
+.list-box
+    display flex
+    flex-wrap wrap
+    justify-content space-between
+    margin 30px 0 70px 0
+    &-item
+        width calc(50% - 10px)
+        padding 20px 30px 20px 20px
+        margin-bottom  20px
+        display flex
+        text-decoration none!important
+        color #666
+        background #fff
+        border 1px solid #E6ECF0
+        border-radius 10px
+        box-sizing border-box
+        &:hover
+            cursor pointer
+            border 1px solid #0173EB
+            box-shadow: 0 0 20px 0 rgba(0, 0, 0, .1)
+            > div:last-of-type p:first-of-type
+                color #0173EB
+        > div:first-of-type
+            width 60px
+            padding-top 4px
+            img
+                width 40px
+        > div:last-of-type
+            flex 1
+            p:first-of-type
+                font-weight 500
+                color #000
+                font-size 20px
+                margin-bottom 10px
+table
+    display table
+    table-layout fixed
+    border-radius 10px
+    border 1px solid #E6ECF0
+    border-collapse separate
+    td
+        border-bottom 1px solid #E6ECF0
+        border-right 1px solid #E6ECF0
+        &:last-of-type
+            border-right none
+    tr:last-of-type td 
+        border-bottom none
+    tr:first-of-type
+        td:first-of-type
+            border-top-left-radius 10px
+        td:last-of-type
+            border-top-right-radius 10px    
+    *
+        background #F9FAFB!important
+        border: none;
+    div
+        display flex
+        span:first-of-type
+            min-width 70px
+            margin-right 10px
+            font-weight 500
+.list-display
+    display none
+.rule
+    counter-reset section
+    &-item
+        font-size 14px
+        background #abb2bb
+        height 40px
+        line-height 40px
+        padding 0 20px 0 48px
+        border-radius 20px
+        display inline-block
+        position relative
+        margin-bottom 10px
+        margin-right 15px
+        text-overflow ellipsis
+        overflow hidden
+        white-space nowrap
+        box-sizing border-box
+        max-width 100%
+        color #fff
+        &::before
+            position absolute
+            counter-increment section 1
+            content counter(section)
+            width 28px
+            height 28px
+            border-radius 100%
+            background #fff;
+            color #0173EB
+            left 10px
+            top 6px
+            text-align center
+            line-height 28px
+.join-section
+    position relative
+    padding-bottom 64px
+    padding-top 20px!important
+    *
+        position relative
+    &::before
+        content ""
+        position absolute
+        left -40px
+        right -40px
+        top 0
+        bottom 0
+        background-image url(./../assets/join-us.png)
+        background-size cover
+
+p
+    font-size 15px
+    line-height 26px
+    margin 0
+
+table
+    margin 0 0 60px 0
+
+h2
+    font-size 30px
+h3
+    font-size 20px
+h2,
+h3
+    padding 0!important
+    margin 20 0!important
+    border none
+    background transparent
+
+@media (max-width: 719px)
+    .list-page
+        padding-left 32px
+        padding-right 32px
+    .list-box 
+        margin-bottom 20px
+    .list-box-item
+        width 100%
+    table
+        display none
+        table-layout auto
+    .list-display
+        display block
+        padding 0
+        margin 0 0 40px 0
+        border 1px solid #d5d5d5
+        border-radius 10px
+        li
+            display flex
+            list-style none
+            margin 0
+            padding-left 15px
+            height 39px
+            line-height 39px
+            border-bottom 1px solid #E6ECF0;
+            &:last-of-type
+                border-bottom none
+            span:first-of-type
+                width 80px
+                font-size 16px
+                font-weight bolder
+                margin-right 20px
+                display  inline-block
+            span:last-of-type
+                font-size 14px
+                color #333
+</style>
---
+++ .vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,284 @@
+<!-- themePath/layouts/GlobalLayout.vue -->
+<template>
+    <div id="global-layout">
+        <header class="bk-dark">
+            <!-- <video autoplay loop muted>
+                <source src="./../../assets/bk.mp4" type="video/mp4"></source>
+            </video> -->
+            <div>
+                <div class="header-content">
+                    <h1>千言</h1>
+                    <p>LUGE ( Language Understanding and Generation Evaluation Benchmarks )</p>
+                    <p>全面的面向自然语言理解和生成任务的中文开源数据集合，旨在为研究人员带来一站式的数据集浏览、整理、下载和评测的科研体验，共同推动中文信息处理技术的进步。</p>
+                    <div style="display: flex; justify-content: center;">
+                        <a class="btn" href="https://github.com/luge-ai/luge-ai" target="_blank">
+                            <i class="iconfont icon-github"></i>
+                            <span>View on GitHub</span>
+                        </a>
+                        <a class="btn" href="https://gitee.com/luge-ai/luge-ai" target="_blank">
+                            <i class="iconfont icon-gitee"></i>
+                            <span>View on Gitee</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <ul v-if="!tabValue"  class="mes-tab">
+            <li><a href="/dataset.html">其他开源数据集索引</a></li>
+            <li><img src="../../assets/arrow.png"></li>
+            <li>{{mes}}</li>
+        </ul>
+        <div class="tabs" :class="tabsFix ? 'tabs-fix' : ''" v-if="tabValue">
+            <span v-for="(item, index) in tabs" :key="index" :class="item.value === tabValue ? 'tabs-active' : ''" @click="onClickTab(item.path)">{{item.label}}</span>
+        </div>
+        <list-component v-if="tabValue === 1"></list-component>
+        <component :is="layout" v-else></component>
+
+        <footer-component></footer-component>
+    </div>
+</template>
+
+<script>
+import listComponent from './../../components/list';
+import footerComponent from './../../components/footer';
+export default {
+    components: {
+        listComponent,
+        footerComponent
+    },
+    data() {
+        return {
+            tabValue: 1,
+            tabsFix: false,
+            tabs: [
+                {path: '/', label: '千言数据集', value: 1},
+                {path: '/dataset.html', label: '其它开源数据集索引', value: 2}
+            ],
+            mes: ''
+        };
+    },
+    computed: {
+        layout() {
+            if (this.$page.path) {
+                const layout = this.$page.frontmatter.layout;
+                this.mes = this.$page.headers[0].title;
+                if (layout && (this.$vuepress.getLayoutAsyncComponent(layout)
+                    || this.$vuepress.getVueComponent(layout))) {
+                    return layout;
+                }
+                return 'Layout';
+            }
+            return 'NotFound';
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            vm.handleChangeTab(to.path);
+        });
+    },
+    mounted() {
+        window.addEventListener('scroll', () => {
+            const top = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
+            if (top > 288) {
+                this.tabsFix = true;
+            } else {
+                this.tabsFix = false;
+            }
+        });
+    },
+    methods: {
+        handleChangeTab(path) {
+            let hasTab = false;
+            this.tabs.forEach(item => {
+                if (item.path === path) {
+                    this.tabValue = item.value;
+                    hasTab = true;
+                }
+            });
+            if (!hasTab) {
+                this.tabValue = '';
+            }
+            this.$page.hasTab = this.tabValue !== '';
+        },
+        onClickTab(path) {
+            if (this.$router.currentRoute.path !== path) {
+                this.$router.push(path);
+            }
+        }
+    }
+};
+</script>
+<style lang='stylus' scoped>
+@import url("./../../assets/icon/iconfont.css");
+#global-layout
+    min-height 100%
+    // padding-bottom 需要动态计算footer
+
+header + footer,
+.tabs + footer
+    margin-bottom 400px
+    z-index -1
+
+.bk-dark
+    background-color  #0a1d3a
+    background-image url(./../../assets/bg1.png)
+    background-position center
+    background-size cover
+    color #fff
+h1, h2
+    background transparent
+    border none
+    padding 0!important
+    margin 0 !improtant
+header
+    text-align center
+    height 288px
+    position relative
+    overflow hidden
+    > div
+        width 100%
+        height 100%
+        display flex
+        justify-content center
+        align-items center
+    .header-content
+        z-index 1
+        width 800px
+        text-align center
+        font-size 14px
+        h1
+            line-height 66px
+            font-size 48px
+            margin 0 !important
+        p
+            width 600px
+            line-height 26px
+            margin auto
+            &:nth-of-type(1)
+                font-weight 300
+                margin-bottom 10px
+        div
+            text-align center
+            margin-top 20px
+            a:nth-of-type(1)
+                margin-right 50px
+
+
+    video
+        width 100%
+        height 100%
+        position absolute
+        left 50%
+        top 50%
+        transform translate3d(-50%, -50%, 0)
+        width 1440px
+        height 288px
+
+.btn
+    height 40px
+    border 1px solid #fff
+    color #fff
+    line-height 38px
+    display inline-block
+    border-radius 20px
+    font-weight 400
+    width 170px
+    display flex
+    align-items center
+    justify-content center
+    i
+        margin-right 5px
+        font-size 20px
+    &:hover
+        background #fff
+        color #071B34
+.tabs
+    height 60px
+    line-height 60px
+    text-align center
+    background #fff
+    box-shadow 0 2px 10px 0 rgba(0, 0, 0, .05)
+    font-size 20px
+    color #333
+    &-fix {
+        position fixed
+        top 0
+        left 0
+        right 0
+        z-index 1
+    }
+    span
+        padding 0 5px
+        margin-right 80px
+        border-bottom 2px solid transparent
+        height 100%
+        box-sizing border-box
+        display inline-block
+        cursor pointer
+        &:last-of-type
+            margin-right 0
+        &:hover,
+        &.tabs-active
+            color #0173EB
+            border-color #0173EB
+.mes-tab 
+    display flex
+    max-width 800px
+    margin 0 auto
+    padding-top 30px
+    padding-left 0
+    li 
+        margin 0
+        padding 0
+        list-style none
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        &:first-of-type
+            margin-right 10px
+            a
+                color #666
+                &:hover
+                    color #0173eb
+        &:last-of-type
+            margin-left 10px
+            color #333
+        img
+            width 8px
+@media (max-width: 719px)
+    .bk-dark
+        background-color  #0a1d3a
+        background-image url(./../../assets/bg2.png)
+        background-position center
+        background-size cover
+    header
+        .header-content
+            width 100%
+            box-sizing border-box
+            padding 0 1.5rem
+            font-size 12px
+            h1
+                font-size 30px
+                line-height 48px
+                margin 0 !important
+            p
+                width 100%
+                margin-bottom 0!important
+            div
+                margin-left -2rem
+                margin-right -2rem
+            a:first-of-type
+                margin-right 10px!important
+
+        .btn
+            font-size 12px
+            height 28px
+            line-height 26px
+            width 140px
+    .tabs
+        display flex
+        justify-content space-around
+        span 
+            margin-right 0
+
+</style>
